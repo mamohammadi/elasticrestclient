e92d7d2c91e48084fabb02133f586b498265e9f0
MohammdAmin Mohammadi <amin.game12@gmail.com>
2022-06-28 21:02:01 +0430

    Added string to primitive types converter methods

---
+++ src/main/java/com/github/mamfj/elasticrestclient/converters/StringToPrimitiveTypeConverter.java
@@ -0,0 +1,17 @@
+package com.github.mamfj.elasticrestclient.converters;
+
+import java.beans.PropertyEditor;
+import java.beans.PropertyEditorManager;
+import java.util.Date;
+
+public class StringToPrimitiveTypeConverter {
+    public static <T> T convert(String valueStr, Class<T> tClass){
+        if(tClass == Date.class){
+            return (T)DateUtil.toDate(valueStr);
+        }else {
+            PropertyEditor editor = PropertyEditorManager.findEditor(tClass);
+            editor.setAsText(valueStr);
+            return (T)editor.getValue();
+        }
+    }
+}
