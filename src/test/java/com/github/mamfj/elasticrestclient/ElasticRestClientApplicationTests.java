e8abde7b35d710f0a350ea0c2e0f36b21fc288f4
MohammdAmin Mohammadi <amin.game12@gmail.com>
2022-06-28 20:33:57 +0430

    Init

---
+++ src/test/java/com/github/mamfj/elasticrestclient/ElasticRestClientApplicationTests.java
@@ -0,0 +1,26 @@
+package com.github.mamfj.elasticrestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.core.*;
+import org.springframework.data.elasticsearch.core.cluster.ClusterOperations;
+import org.springframework.data.elasticsearch.core.convert.ElasticsearchConverter;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+import org.springframework.data.elasticsearch.core.query.*;
+import org.springframework.data.elasticsearch.core.reindex.ReindexRequest;
+import org.springframework.data.elasticsearch.core.reindex.ReindexResponse;
+import org.springframework.data.elasticsearch.core.routing.RoutingResolver;
+
+import java.util.List;
+
+@SpringBootTest()
+class ElasticRestClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}0
